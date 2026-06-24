a3a4bbfbee1d3bdbe7f436588c437aa416b4d479
Caro-Brandon <1brandoncaro1@gmail.com>
2026-06-24 17:24:39 -0300

    .

---
+++ EjercicioN-4/ejercicios.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double baseRect = 5;
+        double altura = 3;
+
+        double area = baseRect * altura;
+        Console.WriteLine("area: " + area);
+
+        int resto = 100 % 7;
+        Console.WriteLine("resto: " + resto);
+        
+        double celsius = 30;
+        double fahrenheit = celsius * 9 / 5 + 32;
+
+        Console.WriteLine("Fahrenheit: " + fahrenheit);
+
+        string numeroTexto = "12";
+        int numero = int.Parse(numeroTexto);
+        Console.WriteLine("numero convertido: " + numero);
+
+        string textoDouble = "12.5";
+        double valor;
+
+        if (double.TryParse(textoDouble, out valor))
+            {
+                Console.WriteLine("conversion es: " + valor);
+            }
+            else
+            {
+                Console.WriteLine("error al convertir");
+            }
+
+        int precio = 1000;
+        double precioFinal = precio * 1.21;
+        Console.WriteLine("precio final: " + precioFinal);
+
+        int divisionEntera = 5 / 2;
+        double divisionReal = 5.0 / 2.0;
+        Console.WriteLine("division entera: " + divisionEntera);
+        Console.WriteLine("division real: " + divisionReal);
+
+        int contador = 0;
+        contador++;
+        Console.WriteLine("contador: " + contador);
+
+        double numeroDecimal = 9.8;
+        int numeroEntero = (int)numeroDecimal;
+
+        Console.WriteLine("Entero: " + numeroEntero);
+
+        int horas = 1000;
+        int dias = horas / 24;
+
+        Console.WriteLine("dias completos: " + dias);
+
+    }
+}
---
+++ TP3/ejercicios.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string nombre = "Brandon";
+        int edad = 17;
+        double altura = 1.68;
+        string ciudad = "caba";
+
+        var numero = 10;
+        var texto = "Hola";
+        var decimalNum = 2.12;
+
+        Console.WriteLine(numero.GetType());
+        Console.WriteLine(texto.GetType());
+        Console.WriteLine(decimalNum.GetType());
+
+        bool esMayorDeEdad;
+        if(edad >= 18)
+        {
+            esMayorDeEdad = true;
+        }
+        else
+        {
+            esMayorDeEdad = false;
+        }
+
+        char inicial = 'B';
+
+        Console.WriteLine(nombre);
+        Console.WriteLine(edad);
+        Console.WriteLine(altura);
+        Console.WriteLine(ciudad);
+        Console.WriteLine(esMayorDeEdad);
+        Console.WriteLine(inicial);
+
+        Console.WriteLine($"Me llamo {nombre} y tengo {edad} años");
+
+        //int texto = "para ver un error ";
+        //string noPonerNda;
+        //Console.WriteLine(noPonerNda);
+        
+        Console.ReadKey();
+    }
+}
